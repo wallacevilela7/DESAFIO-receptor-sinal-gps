ee48abbd58fd89a3e3b633a6bf77d36b970eceed
Wallace Vilela <wallace.vilela9@gmail.com>
2024-12-12 19:48:26 -0300

    Create Spring app

---
+++ src/main/java/wvsdev/receptorgps/ReceptorGpsApplication.java
@@ -0,0 +1,13 @@
+package wvsdev.receptorgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptorGpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptorGpsApplication.class, args);
+    }
+
+}
